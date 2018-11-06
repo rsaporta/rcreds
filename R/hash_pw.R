0617a434e229248bc858c365e25df04500e1355d
Rick Saporta on Vydia_MBPbar18 <rick.saporta@vydia.com>
2018-11-05 21:23:39 -0500

    hash_pw - documentation

--- R/hash_pw.R
+++ R/hash_pw.R
@@ -1,3 +1,42 @@
+# ========================================================== #
+#    hash_pw.R                                               #
+# ========================================================== #
+#
+#' Hashing functions
+#'
+#' Hashes passwords using a variety of algorithms
+#'
+#' Wrappers to digest package, using algorithsm made available via digest.
+#' Specifically, does \emph{not} serialize the hash.
+#'
+#' @name hash_pw
+#'
+#X## -------------------------------  PARAMS  ------------------------------- ##
+#' @param pw string to hash. Cannot be blank or blank-like
+#'
+#' @param algo algorithm to use. Passed to the digest `algo` parameter
+#'             see `?digest::digest` for more.
+#'
+#X## ------------------------------------------------------------------------ ##
+#'
+#' @return
+#'    The hashed string.
+#' 
+#' @examples
+#'
+#' #'  \dontrun{
+#'    library(rcreds)
+#' 
+#'    hash_pw_md5("P4ssword!")
+#'    hash_pw("P4ssword!", algo="md5")
+#' 
+#' }
+#'
+NULL
+
+
+#' @rdname hash_pw
+#' @importFrom magrittr %>%
 #' @export
 hash_pw <- function(pw, algo) {
   requireNamespace("digest")
@@ -8,19 +47,24 @@ hash_pw <- function(pw, algo) {
   digest::digest(pw, algo=algo, serialize=FALSE, length=Inf, file=FALSE)
 }
 
+#' @rdname hash_pw
+#' @importFrom magrittr %>%
 #' @export
 hash_pw_md5 <- function(pw) {
   hash_pw(pw=pw, algo="md5")
 }
 
+#' @rdname hash_pw
+#' @importFrom magrittr %>%
 #' @export
 hash_pw_sha1 <- function(pw) {
   hash_pw(pw=pw, algo="sha1")
 }
 
+#' @rdname hash_pw
+#' @importFrom magrittr %>%
 #' @export
 hash_pw_sha512 <- function(pw) {
   hash_pw(pw=pw, algo="sha512")
 }
-
 
